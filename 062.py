498f4626ad82535fc64e35eed861aa8dd2fa4507
Rodrigoavf <ravf@outlook.com>
2023-08-05 10:18:00 -0300

    Ajuste no código para o enunciado do problema

--- 062.py
+++ 062.py
@@ -6,23 +6,24 @@ E pergunte ao usuário se ele quer ver o próximo número da PA
 Encerre o programa quando o usuário disser não
 '''
 
+adicional = 10
+inicial = 0
+
+print(f'{"CALCULADORA DE PA":-^50}')
+
 n = int(input('Insira o primeiro número de uma PA: '))
 r = int(input('Informe a razão dessa PA: '))
 
 print('Os 10 primeiro números desta PA são: ')
-i = 0
-c = 'S'
-while i < 10:
-        print(n+(r*i))
-        i += 1
-while c != 'N':
-    c = str(input('Deseja ver o próximo númeo da PA? [S/N]')).upper()
-    match c:
-        case 'S':
-            i += 1
-            print(f'O {i}º número desta PA é: {n+(r*i)}')
-        case 'N':
-            print('Tchau')
-        case _:
-            print('Opção não válida')
-    
+while inicial < 10:
+    print(n+r*inicial)
+    inicial += 1
+
+while adicional > 0:
+    adicional = int(input('Deseja ver mais quantos números desta PA? '))
+    if adicional == 0:
+        print(f'{"PROGRAMA ENCERRADO":-^50}')
+        break
+    for i in range(inicial,inicial+adicional):
+        print(n+r*i)
+    inicial += adicional
